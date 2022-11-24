e1ba53a9d36ac6fe6b4484411de9d1572f53b12d
soporhj <desistoeu32@gmail.com>
2022-11-23 22:37:24 -0300

    criacao de menu

--- principal.php
+++ principal.php
@@ -1,3 +1,42 @@
-<h1>principal</h1>
-  <h2>titulo</h2>
-<p>texto</p>
+<style type="text/css">
+    td{
+      padding: 10px;
+      margin: 10px;
+    }
+  </style>
+<table>
+<td>
+        <th>
+          <a href="#">Navbar</a>
+        </th>
+    </td>
+    <td>
+        <th>
+          <a href="#">Home</a>
+      </th>
+    </td>
+    <td>
+        <th>
+          <a href="#">Link</a>
+        </th>
+    </td>
+  <!-- <div>
+    
+    <button type="button">
+      <span> span</span>
+    </button>
+    <div id="navbarSupportedContent">
+      <ul>
+        <li>
+          
+        </li>
+        <li>
+          
+        </li>
+        <li>
+          <a>Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div> -->
+</table>
